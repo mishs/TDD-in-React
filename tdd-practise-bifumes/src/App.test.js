1e06feb4e3db576d5f31911930309f65e452e99c
mishs <mishuct26@gmail.com>
2020-03-23 14:11:46 +0200

    Imports Enzyme configuration, adapter and shallow to use Enzyme testing way

--- tdd-practise-bifumes/src/App.test.js
+++ tdd-practise-bifumes/src/App.test.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+//setting up Enzyme
+import { configure, shallow } from "enzyme"; //configure for the Enzyme setting up and shallow -enzyme function that creates an instance of your component as a unit without mistakenly asserting behaviou of child components
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() }); //adapter has to be the new version of the imported
 
 //jest API methods that organises test code in blocks vs test method
 describe("Counter testing", () => {
   test("renders the title of the counter", () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/This is a counter app/i);
-    expect(linkElement).toBeInTheDocument();
+    const wrapper = shallow(<App />); //Naming 'wrapper is a convention you can name it anything you want. And shallow insures though they be children inside app (like commented out Header App element), it won't look into them.
+    //console.log(wrapper.debug()); //to see what's inside the wrapper
+    expect(wrapper.find("hi").text()).toContain("This is counter app");
+    // const { getByText } = render(<App />);
+    // const linkElement = getByText(/This is a counter app/i);
+    // expect(linkElement).toBeInTheDocument();
   });
 });
