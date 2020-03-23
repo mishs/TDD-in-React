6f1a9e50ca159041e62b2bacddb394f7f27a888c
mishs <mishuct26@gmail.com>
2020-03-23 15:16:59 +0200

    takes configuration of every test file of the application

--- tdd-practise-bifumes/src/setupTests.js
+++ tdd-practise-bifumes/src/setupTests.js
@@ -2,4 +2,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
+import { configure } from "enzyme"; //configure for the Enzyme setting up and shallow -enzyme function that creates an instance of your component as a unit without mistakenly asserting behaviou of child components
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() }); //adapter has to be the new version of the imported
