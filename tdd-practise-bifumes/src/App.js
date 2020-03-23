913a4f6415929ea5b694c5024a1f310b80e3dbb0
mishs <mishuct26@gmail.com>
2020-03-23 14:52:05 +0200

    State test of the counter made to now pass

--- tdd-practise-bifumes/src/App.js
+++ tdd-practise-bifumes/src/App.js
@@ -3,9 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [counter, setcounter] = React.useState(0);
   return (
     <div className="App">
       <h1>This is a counter app</h1>
+      <div id="counter-value">{counter}</div>
       <button id="increment-btn">Increment</button>
     </div>
   );
