aa3512c1c225331c6182e8ab5289bfa2c9f0c44a
mishs <mishuct26@gmail.com>
2020-03-23 14:34:40 +0200

    updates tests to suit aspect being served

--- tdd-practise-bifumes/src/App.test.js
+++ tdd-practise-bifumes/src/App.test.js
@@ -9,16 +9,16 @@ configure({ adapter: new Adapter() }); //adapter has to be the new version of th
 
 //jest API methods that organises test code in blocks vs test method
 describe("Counter testing", () => {
-  test("renders the title of the counter", () => {
+  test("render the title of the counter", () => {
     const wrapper = shallow(<App />); //Naming 'wrapper is a convention you can name it anything you want. And shallow insures though they be children inside app (like commented out Header App element), it won't look into them.
     //console.log(wrapper.debug()); //to see what's inside the wrapper
-    expect(wrapper.find("hi").text()).toContain("This is counter app");
+    expect(wrapper.find("h1").text()).toContain("This is a counter app");
     // const { getByText } = render(<App />);
     // const linkElement = getByText(/This is a counter app/i);
     // expect(linkElement).toBeInTheDocument();
   });
 
-  test("render button with text `increment`", () => {
+  test("render a button with text `increment`", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("#increment-btn").text()).toBe("Increment");
   });
