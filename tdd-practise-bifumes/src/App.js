89897997fb35fc13455809553a3904d0473bc103
mishs <mishuct26@gmail.com>
2020-03-23 15:29:38 +0200

    Refactoring code, counter component imported to App for clean coding

--- tdd-practise-bifumes/src/App.js
+++ tdd-practise-bifumes/src/App.js
@@ -1,19 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Counter from "./Counter";
 
 function App() {
-  const [counter, setcounter] = React.useState(0);
   return (
     <div className="App">
-      <h1>This is a counter app</h1>
-      <div id="counter-value">{counter}</div>
-      <button id="increment-btn" onClick={() => setcounter(counter + 1)}>
-        Increment
-      </button>
-      <button id="decrement-btn" onClick={() => setcounter(counter - 1)}>
-        Increment
-      </button>
+      <Counter />
     </div>
   );
 }
